a55aa9e07691be1af22701689eef0711f09749f6
Tosin Afolabi <psyooa@nottingham.ac.uk>
2013-08-04 18:35:51 +0100

    Initial Commit

---
+++ Procuses/PEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PEAppDelegate.h
+//  Procuses
+//
+//  Created by Tosin Afolabi on 04/08/2013.
+//  Copyright (c) 2013 Tosin Afolabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
